fb74c3ea205cf9ec4b74d09104a784866b1b1fb5
BaptisteCtldWbr <baptiste.catelandwambre@gmail.com>
2025-02-10 14:51:06 +0100

    Invulnérabilité - hurt

--- S4-Unity/Assets/Scripts/PlayerHealth.cs
+++ S4-Unity/Assets/Scripts/PlayerHealth.cs
@@ -1,20 +1,47 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int maxLifePoints = 3;
     public int currentLifePoints = 3;
+    public bool isInvulnerable = false;
+    public float invulnerableTime = 2.25f;
+    public float invulnerableFlash = 0.2f;
+    public SpriteRenderer sr;
 
     void Start(){
         currentLifePoints = maxLifePoints;
     }
 
     public void Hurt(int damage = 1){
+        if (isInvulnerable){
+            return;
+        }
         currentLifePoints = currentLifePoints - damage;
         Debug.Log("Perte de "+damage+"pts de vie - Totale à "+ currentLifePoints);
         if(currentLifePoints <= 0){
             Debug.Log("sale naze");
             Destroy(gameObject);
+        } else {
+            StartCoroutine(Invulnerable());
         }
+    }
+
+    IEnumerator Invulnerable(){
+        Color startColor = sr.color;
+        isInvulnerable = true;
+
+        WaitForSeconds invulnerableFlashWait = new WaitForSeconds(invulnerableFlash);
+        for (float i = 0; i <= invulnerableTime; i += invulnerableFlash){
+            if(sr.color.a == 1){
+                sr.color = Color.clear;
+            } else {
+                sr.color = startColor;
+            }
+            yield return new WaitForSeconds(invulnerableFlash);
+        }
+        sr.color = startColor;
+        isInvulnerable = false;
     }
 }
